8aed8e305d173c64d614753fdb727ef7b87ffa7d
LucaBla <me@lucablazevic.dev>
2023-03-06 15:41:29 +0100

    add param mitify_user_id to error_report_params

--- app/controllers/api/v1/skript_reports_controller.rb
+++ app/controllers/api/v1/skript_reports_controller.rb
@@ -56,6 +56,6 @@ class Api::V1::SkriptReportsController < ApplicationController
     def skript_report_params
       params.require(:skript_report).permit(:reportType, :module, :description, :status, :priority, :author,
                                             :eMail, :page, :chapter, :illustrationNumber, :tableNumber,
-                                            :report_date, :granted_date, :completed_date)
+                                            :report_date, :granted_date, :completed_date, :mitify_user_id)
     end
 end
