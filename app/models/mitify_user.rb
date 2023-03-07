05d48d440f8fab67f2a2b720ab08da8e42132979
LucaBla <me@lucablazevic.dev>
2023-03-07 05:31:55 +0100

    remove registerable from mitify_user (so no user creation through api)

--- app/models/mitify_user.rb
+++ app/models/mitify_user.rb
@@ -1,7 +1,7 @@
 class MitifyUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
   
   has_many :error_reports
@@ -10,6 +10,6 @@ class MitifyUser < ApplicationRecord
   has_and_belongs_to_many :attended_modules, class_name: "UniversityModule",
                           join_table: "attendees_university_modules", foreign_key: "attendee_id",
                           association_foreign_key: "attended_module_id"
-                          
+
   belongs_to :role, default: ->{Role.first}
 end
